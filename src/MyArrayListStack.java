5c30cef86200462723bf2198075523af973d9aaa
Maksat <220451@astanait.edu.kz>
2023-04-27 14:53:32 +0600

    .

--- src/MyArrayListStack.java
+++ src/MyArrayListStack.java
@@ -7,6 +7,11 @@ public class MyArrayListStack<T> extends MyArrayLIst {
     public void push(T item){
         arr.addFirst(item);
     }
+    public T pop(){
+        T element = peek(); // get the element at the top of the stack
+        removeFirst(); // remove the element at the top of the stack
+        return element; // return the element that was removed
+    }
     public T peek(){
         if(isEmpty()){
             throw new EmptyStackException();
